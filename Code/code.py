85fc4e88b18eda359c8541c64a1a7621f1960488
Sahil Vankudre <sahilgaming1726@gmail.com>
2026-05-22 15:02:19 +0530

    q-7 easy

--- Code/code.py
+++ Code/code.py
@@ -166,3 +166,27 @@ Input: strs = ["Hello","World"]
 
 Output: ["Hello","World"]
 '''
+
+
+'''
+Q.7) [E] Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+
+Example 1:
+
+Input: s = "Was it a car or a cat I saw?"
+
+Output: true
+'''
+def isPalindrome(self, s: str) -> bool:
+        res = "".join(char.lower() for char in s if char.isalnum())
+        l,r = 0,len(res)-1
+        while l<r:
+            if res[l] != res[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
